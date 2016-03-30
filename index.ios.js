007c581fceb14f8f98e396537cdcb84d3b292ba5
Jimmy Lin <usmcjimmy@gmail.com>
2016-03-30 19:48:46 -0400

    functioning profie, repos and notes, module error with apis, need to resolve the issue

--- index.ios.js
+++ index.ios.js
@@ -1,31 +1,25 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
+var React = require('react-native');
+var Main = require('./App/Components/Main');
 
-import React, {
+var {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  NavigatorIOS,
+  View,
+} = React;
+
 
 class githubNotetaker extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <NavigatorIOS
+      style={styles.container}
+      initialRoute={{
+        title: 'Github Notetaker',
+        component: Main,
+      }}/>
     );
   }
 }
@@ -33,20 +27,9 @@ class githubNotetaker extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    backgroundColor: '#111111',
+  }
 });
+
 
 AppRegistry.registerComponent('githubNotetaker', () => githubNotetaker);
